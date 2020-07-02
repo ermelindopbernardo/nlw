ac55ad07b1520754dae53563aa46f9c11fdfeb3e
Ermelindo Bernardo <benardotooroffice@gmail.com>
2020-07-02 11:01:22 +0100

    Erro no Model
    Arranjar o model

--- script/create-point.js
+++ script/create-point.js
@@ -26,11 +26,15 @@
 
         const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`
         console.log(citySelect)
+        citySelect.innerHTML = "<option value> Selecione a Cidade</option>"
+        citySelect.disabled = true
+        
         fetch(url)
         .then(res => res.json())
         .then( cities => {
+            
             for(const city of cities){ 
-                citySelect.innerHTML += `<option value="${city.id}">${city.nome}</option>`;
+                citySelect.innerHTML += `<option value="${city.nome}">${city.nome}</option>`;
             } 
             citySelect.disabled = false
         })
@@ -40,3 +44,46 @@
     document
         .querySelector("select[name=uf]")
         .addEventListener("change", getCities)
+
+///Items de Coleta
+//Pegar dos os Li's
+
+const itemnsTocollect = document.querySelectorAll(".items-grid li")
+
+for(const item of itemnsTocollect){
+    item.addEventListener("click", handleSelectedItem)
+}
+
+const collectedItems = document.querySelector("input[name=items]")
+ 
+let selectedItems = []
+
+
+function handleSelectedItem(event){
+    const itemLi = event.target
+    // addremove uma class com javascript
+    itemLi.classList.toggle("selected")
+    const itemId = itemLi.dataset.id
+    
+    const AlreadySelected = selectedItems.findIndex(item =>{
+        const itemFound = item == itemId
+        return itemFound 
+    })
+
+    if (AlreadySelected >= 0) {
+        const filteredItems = selectedItems.filter(item =>{
+            const itemIsDifferent = item != itemId
+            return itemIsDifferent
+        })
+        
+        selectedItems  = filteredItems
+    }else{
+        
+
+        selectedItems.push(itemId)
+ 
+    }
+
+    collectedItems.value = selectedItems
+}
+
